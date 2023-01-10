97584f7f48844d70c70c5f410ba3c053d783a9df
Toche Camille <tochecamille@gmail.com>
2023-01-10 01:53:09 +0100

    Update cover states

--- custom_components/virtual/cover.py
+++ custom_components/virtual/cover.py
@@ -2,6 +2,7 @@
 This component provides support for a virtual cover.
 
 """
+from time import sleep
 
 import logging
 
@@ -16,14 +17,13 @@ from homeassistant.components.cover import (
     CoverEntity,
 )
 
-# TODO: during state
 from homeassistant.const import (
     STATE_CLOSED,
     STATE_CLOSING,
     STATE_OPEN,
     STATE_OPENING,
-    Platform,
 )
+
 _LOGGER = logging.getLogger(__name__)
 
 CONF_NAME = "name"
@@ -31,17 +31,12 @@ CONF_INITIAL_VALUE = "initial_value"
 CONF_INITIAL_POSITION = "initial_position"
 CONF_INITIAL_AVAILABILITY = "initial_availability"
 
-DEFAULT_INITIAL_VALUE = "open"
-DEFAULT_INITIAL_POSITION = 100
-DEFAULT_INITIAL_AVAILABILITY = True
-
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_NAME): cv.string,
-    vol.Optional(CONF_INITIAL_VALUE, default=DEFAULT_INITIAL_VALUE): cv.string,
-    vol.Optional(CONF_INITIAL_POSITION, default=DEFAULT_INITIAL_POSITION): cv.positive_int,
-    vol.Optional(CONF_INITIAL_AVAILABILITY, default=DEFAULT_INITIAL_AVAILABILITY): cv.boolean,
+    vol.Optional(CONF_INITIAL_VALUE, default="open"): cv.string,
+    vol.Optional(CONF_INITIAL_POSITION, default=100): cv.positive_int,
+    vol.Optional(CONF_INITIAL_AVAILABILITY, default=True): cv.boolean,
 })
-
 
 async def async_setup_platform(_hass, config, async_add_entities, _discovery_info=None):
     locks = [VirtualCover(config)]
@@ -61,22 +56,45 @@ class VirtualCover(CoverEntity):
             self._name = self.name[1:]
         self._unique_id = self._name.lower().replace(' ', '_')
 
-        self._state = config.get(CONF_INITIAL_VALUE)
+        self._state = STATE_CLOSED if config.get(CONF_INITIAL_VALUE) == STATE_CLOSED else STATE_OPEN
         self._position = config.get(CONF_INITIAL_POSITION)
+
+        if self._position > 100:
+            self._position = 100
 
         self._available = config.get(CONF_INITIAL_AVAILABILITY)
         _LOGGER.info('VirtualCover: {} created'.format(self._name))
+    
+    @property
+    def state_attributes(self):
+        """Return the state attributes."""
+        data = {}
+
+        data[ATTR_CURRENT_POSITION] = self._position
+        data[ATTR_CURRENT_POSITION] = self._position
+
+        return data
 
 
     @property
     def is_closed(self):
         """Return if the cover is closed."""
-        return self._state == 'closed'
+        return self._state == STATE_CLOSED
+
+    @property
+    def is_closing(self):
+        """Return if the cover is closing."""
+        return self._state == STATE_CLOSING
 
     @property
     def is_open(self):
         """Return if the cover is open."""
-        return self._state == 'open'
+        return self._state == STATE_OPEN
+
+    @property
+    def is_opening(self):
+        """Return if the cover is opening."""
+        return self._state == STATE_OPENING
 
     def close_cover(self):
         """Close the cover."""
@@ -88,11 +106,20 @@ class VirtualCover(CoverEntity):
 
     def set_cover_position(self, position: int = 0, **kwargs):
         """Move the roller shutter to a specific position."""
+        if position == self._position:
+            return
+        
+        if position > 100:
+            position = 100
+
+        self._state = STATE_CLOSING if position < self._position else STATE_OPENING
+        sleep(4)
+
         if position <= 0:
             self._position = 0
-            self._state = 'closed'
+            self._state = STATE_CLOSED
         else:
-            self._state = 'open'
+            self._state = STATE_OPEN
             self._position = position
 
 
